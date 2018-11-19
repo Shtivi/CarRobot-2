250630d328cfd5a2f1ca3273326e987f7a8d3141
Shtivi <idoshtivi@gmail.com>
2018-11-20 00:17:02 +0200

    removed previous files

--- car-robot-app/src/components/HelloWorld.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Vue, Prop } from 'vue-property-decorator'
-import WithRender from './HelloWorld.template.html';
-
-@Component
-@WithRender
-export default class HelloWorld extends Vue {
-    @Prop() msg!: string;
-}
