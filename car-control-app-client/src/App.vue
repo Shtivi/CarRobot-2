795ae0cdeed67ee83c3b36d6f3a61e3a5d6b2241
Shtivi <idoshtivi@gmail.com>
2018-11-21 18:15:20 +0200

    progress in client project

---
+++ car-control-app-client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <driver-cabin></driver-cabin>
+  </div>
+</template>
+
+<script>
+import DriverCabin from './components/DriverCabin.vue';
+
+export default {
+  name: 'app',
+  components: {
+    DriverCabin
+  }
+}
+</script>
+
+<style lang="scss">
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
---
+++ car-control-app-client/src/components/DriverCabin.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="driver-cabin">1
+        <navigation-control side='left' v-bind:controls="navigationControls.driving"></navigation-control>
+        <navigation-control side='right' v-bind:controls="navigationControls.cameraTilt"></navigation-control>
+    </div>
+</template>
+
+<script>
+import NavigationControl from './NavigationControl';
+
+export default {
+    name: 'DriverCabin',
+    components: {
+        NavigationControl
+    },
+    data() {
+        return {
+            navigationControls: {
+                driving: [
+                    {
+                        icon: 'keyboard_arrow_up',
+                        index: 1
+                    }, 
+                    {
+                        icon: 'thumb_up',
+                        index: 2
+                    }, 
+                    {
+                        icon: 'thumb_up',
+                        index: 3
+                    }, 
+                    {
+                        icon: 'thumb_up',
+                        index: 4
+                    }
+                ],
+                cameraTilt: [
+                    {
+                        icon: ''
+                    }, 
+                    {
+                        icon: ''
+                    }, 
+                    {
+                        icon: ''
+                    }, 
+                    {
+                        icon: ''
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.driver-cabin {
+    background-image: url('../assets/images.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+    background-color: #fff;
+    color: white;
+}
+</style>
