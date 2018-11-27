37df6d8fb2e966437d7ae4a3a35bd842fa621988
Shtivi <idoshtivi@gmail.com>
2018-11-27 13:56:41 +0200

    progress in python

--- RPiControlAgent/external/serialCommunicator.py
+++ RPiControlAgent/external/serialCommunicator.py
@@ -2,19 +2,23 @@ import sys
 import os
 import time 
 import json
-# import serial
+import serial
 
-# serial = serial.Serial(os.environ['port'])
+serial = serial.Serial(os.environ['port'])
 
 sys.stdout.write("READY")
 sys.stdout.flush()
 
 while True:
-    data = sys.stdin.readline()
+    cmd = sys.stdin.readline()
     try:
-        if len(data) > 0:
-            raise Exception('got an input!')
-            # ser.write(data)
+        if (len(cmd) > 0):
+            serial.write(cmd)
+            serial.flush()
+        incomingMsg = serial.readline()
+        if (len(incomingMsg) > 0):
+            sys.stdout.write(json.dumps({"incoming": incomingMsg}))
+            sys.stdout.flush()
     except Exception as err:
         sys.stderr.write(json.dumps({"error": str(err)}))
         sys.stderr.flush()
