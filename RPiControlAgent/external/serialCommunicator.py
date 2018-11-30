69a0377fa5268cdad3ecb19f8b819a5718ac2a4c
Ido Shtivi <idoshtivi@gmail.com>
2018-11-30 16:01:54 +0200

    fixes in python'

--- RPiControlAgent/external/serialCommunicator.py
+++ RPiControlAgent/external/serialCommunicator.py
@@ -2,23 +2,23 @@ import sys
 import os
 import time 
 import json
-import serial
+# import serial
 
-serialPort = serial.Serial(os.environ['port'])
+# serialPort = serial.Serial(os.environ['port'])
 
 sys.stdout.write("READY")
 sys.stdout.flush()
 
 while True:
-    cmd = sys.stdin.readline()
+    cmd = sys.stdin.readline().strip()
     try:
         if (len(cmd) > 0):
             serialPort.write(cmd)
             serialPort.flush()
-        incomingMsg = serialPort.readline()
-        if (len(incomingMsg) > 0):
-            sys.stdout.write(json.dumps({"incoming": incomingMsg}))
-            sys.stdout.flush()
+        # incomingMsg = serialPort.readline()
+        # if (len(incomingMsg) > 0):
+        #     sys.stdout.write(json.dumps({"incoming": incomingMsg}))
+        #     sys.stdout.flush()
     except Exception as err:
         sys.stderr.write(json.dumps({"error": str(err)}))
         sys.stderr.flush()
